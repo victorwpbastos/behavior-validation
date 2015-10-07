093474d7c8965ab74acb9ba5fa6cc1c53af5c25b
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-10-07 14:07:09 -0300

    support for radio and checkbox inputs in required rule

--- validation/rules/required.js
+++ validation/rules/required.js
@@ -2,8 +2,16 @@ module.exports = function(message) {
 	message = message || 'Value is required';
 
 	return function(field) {
-		if(field.val() === '') {
-			return message;
+		if(field[0].type === 'radio' || field[0].type === 'checkbox') {
+			var attrName = field[0].name;
+
+			if($('[name="' + attrName + '"]').is(':checked') === false) {
+				return message;
+			}
+		} else {
+			if(field.val() === '') {
+				return message;
+			}
 		}
 	};
 };

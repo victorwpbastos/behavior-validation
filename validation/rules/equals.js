49d533f8b04c3e8227c726734ac45be2e041624f
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-10-08 15:16:05 -0300

    support for radio and checkbox inputs in equals rule

--- validation/rules/equals.js
+++ validation/rules/equals.js
@@ -2,8 +2,16 @@ module.exports = function(value, message) {
 	message = message || 'Value must be equals to ' + value;
 
 	return function(field) {
-		if(field.val() != value) {
-			return message;
+		if(field[0].type === 'radio' || field[0].type === 'checkbox') {
+			var attrName = field[0].name;
+
+			if($('[name="' + attrName + '"]:checked').val() != value) {
+				return message;
+			}
+		} else {
+			if(field.val() != value) {
+				return message;
+			}
 		}
 	};
 };
